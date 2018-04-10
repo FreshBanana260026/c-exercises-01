10ac14f7d414a505cfb02c8c7dc9a18445ae8090
Patrik <260026@via.dk>
2018-04-10 10:24:33 +0200

    exercise3

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,10 +1,19 @@
 
 #define F_CPU 10000000L
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
-    DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA3);      // turning on led 0 (active low)
+	DDRA = 0xff;				// setting all I/O's in port A as outputs
+	
+	while (1)
+	{
+		PORTA = ~(1<<PA3);      // turning on led 0 (active low)
+		_delay_ms(500);
+		PORTA = 0xff;			// turning on led 1 (active high)
+		_delay_ms(500);
+	}
+	return 1;
 }
